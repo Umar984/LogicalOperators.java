054653496e41379d5c6467e30cb8f65a1007d13d
Md Umar Faruk <mdumarfaruk793@gmail.com>
2025-02-20 00:23:53 +0530

    Add files via upload

---
+++ logic.java
@@ -0,0 +1,11 @@
+//Logical Operators
+class LogicalOperators{
+public static void main(String args[]){
+
+int a = 11, b = 15;
+System.out.println(a > b && b > 0); 
+System.out.println(a > b || b < 0); 
+System.out.println(!(a > b));    
+
+}
+}
